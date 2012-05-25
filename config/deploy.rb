1f7e0238d967323d64c55be58c9fa923231ceaef
daichi <prinum101@gmail.com>
2012-05-25 16:20:03 +0900

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,3 +55,24 @@ set :default_environment, {
   #   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   # end
 # end
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+
+namespace :deploy do
+    task :start, :roles => :app, :except => { :no_release => true } do 
+        run "cd #{current_path} && BUNDLE_GEMFILE=#{current_path}/Gemfile bundle exec unicorn -c #{unicorn_config} -E #{rails_env} -D"
+    end
+    task :stop, :roles => :app, :except => { :no_release => true } do 
+        run "kill `cat #{unicorn_pid}`"
+    end
+    task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+        run "kill -s QUIT `cat #{unicorn_pid}`"
+    end
+    task :reload, :roles => :app do
+        run "kill -s USR2 `cat #{unicorn_pid}`"
+    end
+    task :restart, :roles => :app, :except => { :no_release => true } do
+        stop
+        start
+    end
+end
